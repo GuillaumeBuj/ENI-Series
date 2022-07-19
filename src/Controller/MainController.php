0875f3ea5c27e2a754538272ded8e6a500f4996b
GuillaumeBuj <guillaume.bujon@gmail.com>
2022-07-19 11:54:10 +0200

    Install Symfony

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -18,6 +18,10 @@ class MainController extends AbstractController
      * @Route("/test",name="main_test")
      */
     public function test(){
-        return $this->render("main/test.html.twig");
+        $serie=[
+            "title"=>"Breaking Bad",
+            "year"=>2000
+        ];
+        return $this->render("main/test.html.twig", ["mySerie"=>$serie]);
     }
 }
